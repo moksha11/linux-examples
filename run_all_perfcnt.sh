f0b39acdbeaa88112898407c1809ab24f6b1c739
sudarsun <sudarsun.kannan@gmail.com>
2014-11-22 23:48:34 -0500

    test

--- run_all_perfcnt.sh
+++ run_all_perfcnt.sh
@@ -4,18 +4,26 @@ HOMEDIR=/home/sudarsun/libs/linux-examples
 
 echo "start harware counter measurement"
 
+cd $HOMEDIR
 cd $HOMEDIR/c-hashtable
+rm -rf SCALINGEXP
+mkdir SCALINGEXP
 scripts/likwid_hashtab_runall.sh
 
+cd $HOMEDIR
 cd $HOMEDIR/btree-1.0
+rm -rf SCALINGEXP
+mkdir SCALINGEXP
 scripts/likwid_hashtab_runall.sh
 
-
+cd $HOMEDIR
 cd $HOMEDIR/binarytree
+rm -rf SCALINGEXP
+mkdir SCALINGEXP
 scripts/likwid_hashtab_runall.sh
 
-cd $HOMEDIR/leveldb-1.14.0
-./level_db_bench_compile.sh compile
-scripts/likwid_hashtab_runall.sh
+#cd $HOMEDIR/leveldb-1.14.0
+#./level_db_bench_compile.sh compile
+#scripts/likwid_hashtab_runall.sh
 
 
