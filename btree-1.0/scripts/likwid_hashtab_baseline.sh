2dc34155de7e23d3673e35d4887a46a87e694403
sudarsun <sudarsun.kannan@gmail.com>
2015-05-20 12:12:43 -0400

    changes

---
+++ btree-1.0/scripts/likwid_hashtab_baseline.sh
@@ -0,0 +1,50 @@
+#!/bin/sh
+HOMEDIR=/home/sudarsun/libs/linux-examples
+DATADIR=/mnt/pmfs
+#NVMDIR=/home/sudarsun/nvmalloc/scripts
+NVMDIR=$NVMALLOC_HOME/scripts
+
+sudo sync
+sudo sh -c "echo 3 > /proc/sys/vm/drop_caches"
+sudo sync
+
+cd ../
+sed -i 's/\-D_ENABL_DATAPERSIST/\-D_NODATAPERSIST/' ./trivial/Makefile
+sed -i 's/\-D_ENABL_DATAPERSIST/\-D_NODATAPERSIST/' ./icount/Makefile
+sed -i 's/\-D_ENABL_DATAPERSIST/\-D_NODATAPERSIST/' ./libpmemalloc/Makefile
+sed -i 's/\-D_ENABL_DATAPERSIST/\-D_NODATAPERSIST/' ./libpmem/Makefile
+sed -i 's/\-D_ENABL_DATAPERSIST/\-D_NODATAPERSIST/' ./Makefile
+sed -i 's/\-D_ENABL_DATAPERSIST/\-D_NODATAPERSIST/' ./util/Makefile
+sed -i 's/\-D_ENABL_DATAPERSIST/\-D_NODATAPERSIST/' ./btree-1.0/Makefile
+sed -i 's/\-D_ENABL_DATAPERSIST/\-D_NODATAPERSIST/' ./basic/Makefile
+sed -i 's/\-D_ENABL_DATAPERSIST/\-D_NODATAPERSIST/' ./Makefile.inc
+
+
+
+sed -i 's/_ENABLE_LOG/_DISABLE_LOG/' trivial/Makefile
+sed -i 's/_ENABLE_LOG/_DISABLE_LOG/' icount/Makefile
+sed -i 's/_ENABLE_LOG/_DISABLE_LOG/' libpmemalloc/Makefile
+sed -i 's/_ENABLE_LOG/_DISABLE_LOG/' libpmem/Makefile
+sed -i 's/_ENABLE_LOG/_DISABLE_LOG/' Makefile
+sed -i 's/_ENABLE_LOG/_DISABLE_LOG/' util/Makefile
+sed -i 's/_ENABLE_LOG/_DISABLE_LOG/' btree-1.0/Makefile
+sed -i 's/_ENABLE_LOG/_DISABLE_LOG/' basic/Makefile
+sed -i 's/_ENABLE_LOG/_DISABLE_LOG/' Makefile.inc
+
+
+sed -i 's/_ENABLE_LOG/_NOPERSIST/' libpmemalloc/pmemalloc.c
+sed -i 's/_ENABLE_LOG/_NOPERSIST/' btree-1.0/bt_code.c
+
+
+#sed -i 's/_DISABLE_LOG/_ENABLE_LOG/' libpmem/pmem_cl.c
+
+make clean
+make -j4
+cd btree-1.0
+make clean
+make -j4
+sudo rm -rf /mnt/pmfs/*
+#sudo fallocate -l 2048M /mnt/pmfs/logfile
+sudo $NVMDIR/likwid_instrcnt.sh "$HOMEDIR/btree-1.0/test 100000"
+
+
